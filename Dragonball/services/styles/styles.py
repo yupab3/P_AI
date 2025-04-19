6d18086485db5597252fb942f0e8a010db83e122
dongyeuk <yupab3@naver.com>
2025-04-19 19:42:42 +0900

    upload

---
+++ Dragonball/services/styles/styles.py
@@ -0,0 +1,27 @@
+import mesop as me
+
+
+SIDENAV_MIN_WIDTH=68
+SIDENAV_MAX_WIDTH=200
+
+DEFAULT_MENU_STYLE = me.Style(align_content="left")
+
+MAIN_COLUMN_STYLE = me.Style(
+    display="flex",
+    flex_direction="column",
+    height="100%",
+)
+
+PAGE_BACKGROUND_STYLE = me.Style(
+    background=me.theme_var("background"),
+    height="100%",
+    overflow_y="scroll",
+    margin=me.Margin(bottom=20),
+)
+
+PAGE_BACKGROUND_PADDING_STYLE = me.Style(
+    background=me.theme_var("background"),
+    padding=me.Padding(top=24, left=24, right=24, bottom=24),
+    display="flex",
+    flex_direction="column",
+)
